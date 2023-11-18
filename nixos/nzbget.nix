d26d5132777233f3770bce0afaa18ca47a66aa65
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-11-18 16:05:05 +0100

    Configure nzbget maindir

--- nixos/nzbget.nix
+++ nixos/nzbget.nix
@@ -1,7 +1,10 @@
-{...}: {
+{config, ...}: {
   services.nzbget = {
     enable = true;
     user = "fausto";
+    settings = {
+      MainDir = "${config.users.users.fausto.home}/nzbget";
+    };
   };
   networking.firewall.allowedTCPPorts = [6789];
 }
