52c1ba0fd8c8685cb0551feccf0a1b566e2ec2c6
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-11-18 20:32:13 +0100

    Use only one caddy host, and use it for Jellyfin

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: let
+{...}: let
   caddyUser = "caddy";
   host = "bilbo.taile08bf.ts.net";
 in {
@@ -10,25 +10,32 @@ in {
     enable = true;
     user = caddyUser;
     virtualHosts = {
-      "jellyfin.${host}".extraConfig = ''
+      "${host}".extraConfig = ''
         reverse_proxy localhost:8096
       '';
 
-      "nzbget.${host}".extraConfig = ''
-        reverse_proxy localhost:6789
-      '';
-
-      "grafana.${host}".extraConfig = ''
-        reverse_proxy localhost:2342
-      '';
-
-      "radarr.${host}".extraConfig = ''
-        reverse_proxy localhost:7878
-      '';
-
-      "sonarr.${host}".extraConfig = ''
-        reverse_proxy localhost:8989
-      '';
+      # The following would be nice but I need to set up subdomains properly
+      # and Tailscale does not support wildcards yet:
+      #
+      # "jellyfin.${host}".extraConfig = ''
+      #   reverse_proxy localhost:8096
+      # '';
+      #
+      # "nzbget.${host}".extraConfig = ''
+      #   reverse_proxy localhost:6789
+      # '';
+      #
+      # "grafana.${host}".extraConfig = ''
+      #   reverse_proxy localhost:2342
+      # '';
+      #
+      # "radarr.${host}".extraConfig = ''
+      #   reverse_proxy localhost:7878
+      # '';
+      #
+      # "sonarr.${host}".extraConfig = ''
+      #   reverse_proxy localhost:8989
+      # '';
     };
   };
 }
