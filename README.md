7939391088c2deb5114e3b6a42e3097deb5fe942
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-11-18 14:44:09 +0100

    Enable and document running in a VM

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Bilbo
 
 My media server.
+
+## Running in a VM
+
+Warning: security measures are waived for the VM in order to facilitate testing.
+
+```sh
+# Build the VM:
+nixos-rebuild build-vm --flake .#bilboVirtual
+
+# Run the VM:
+./result/bin/run-bilbo-vm
+```
+
+After having changed `configuration.nix` or any of its dependencies:
+
+```sh
+# Remove the VM's state:
+rm ./bilbo.qcow2
+```
--- flake.nix
+++ flake.nix
@@ -1,21 +1,29 @@
 {
   description = "fnune's Bilbo—A media server";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
-  };
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+
   outputs = {
     self,
     nixpkgs,
-  }: {
-    nixosConfigurations = {
-      "bilbo" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nixos/configuration.nix
-          ./nixos/hardware-configuration.nix
-          ./nixos/hardware-acceleration.nix
-        ];
-      };
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    bilbo = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./nixos/configuration.nix
+        ./nixos/hardware-configuration.nix
+        ./nixos/hardware-acceleration.nix
+      ];
     };
+    bilboVirtual = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [./nixos/configuration.nix];
+    };
+  in {
+    nixosConfigurations.bilbo = bilbo;
+    nixosConfigurations.bilboVirtual = bilboVirtual;
   };
 }
