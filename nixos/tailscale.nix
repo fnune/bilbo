65e1995a06c746c66b19aefb2bf49a9381cac922
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-11-18 20:03:17 +0100

    Add HTTPS and subdomains with Caddy + TS

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -1,3 +1,34 @@
-{...}: {
-  services.tailscale.enable = true;
+{pkgs, ...}: let
+  caddyUser = "caddy";
+  host = "bilbo.taile08bf.ts.net";
+in {
+  services.tailscale = {
+    enable = true;
+    permitCertUid = caddyUser;
+  };
+  services.caddy = {
+    enable = true;
+    user = caddyUser;
+    configFile = pkgs.writeText "Caddyfile" ''
+      jellyfin.${host} {
+          reverse_proxy localhost:8096
+      }
+
+      nzbget.${host} {
+          reverse_proxy localhost:6789
+      }
+
+      grafana.${host} {
+          reverse_proxy localhost:2342
+      }
+
+      radarr.${host} {
+          reverse_proxy localhost:7878
+      }
+
+      sonarr.${host} {
+          reverse_proxy localhost:8989
+      }
+    '';
+  };
 }
