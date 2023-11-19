1ab2b762e6ad0abd463c1eb4d81e95c606f054de
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-11-19 23:15:37 +0100

    Configure UrlBase for sonarr/radarr

--- nixos/radarr.nix
+++ nixos/radarr.nix
@@ -1,9 +1,16 @@
-{config, ...}: {
+{config, ...}: let
+  dataDir = "${config.users.users.fausto.home}/radarr";
+in {
   services.radarr = {
     enable = true;
     user = "fausto";
     group = "users";
     openFirewall = true;
-    dataDir = "${config.users.users.fausto.home}/radarr";
+    inherit dataDir;
+  };
+  systemd.services.radarr = {
+    preStart = ''
+      sed -i 's|<UrlBase>.*</UrlBase>|<UrlBase>/radarr</UrlBase>|' ${dataDir}/config.xml
+    '';
   };
 }
--- nixos/sonarr.nix
+++ nixos/sonarr.nix
@@ -1,9 +1,16 @@
-{config, ...}: {
+{config, ...}: let
+  dataDir = "${config.users.users.fausto.home}/sonarr";
+in {
   services.sonarr = {
     enable = true;
     user = "fausto";
     group = "users";
     openFirewall = true;
-    dataDir = "${config.users.users.fausto.home}/sonarr";
+    inherit dataDir;
+  };
+  systemd.services.sonarr = {
+    preStart = ''
+      sed -i 's|<UrlBase>.*</UrlBase>|<UrlBase>/sonarr</UrlBase>|' ${dataDir}/config.xml
+    '';
   };
 }
