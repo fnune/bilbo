535e521995b596fdd1fe323e3ede40002fe23a88
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-11-19 22:45:24 +0100

    Add more services

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -11,6 +11,17 @@ in {
     virtualHosts = {
       "bilbo.${host}".extraConfig = ''
         reverse_proxy /grafana/* localhost:2342
+        reverse_proxy /grafana localhost:2342
+
+        reverse_proxy /nzbget/* localhost:6789
+        reverse_proxy /nzbget localhost:6789
+
+        reverse_proxy /radarr/* localhost:7878
+        reverse_proxy /radarr localhost:7878
+
+        reverse_proxy /sonarr/* localhost:8989
+        reverse_proxy /sonarr localhost:8989
+
         reverse_proxy localhost:8096
       '';
     };
