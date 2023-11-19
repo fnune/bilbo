861284dd8b37c6faad36ca9eb516aa0c27382e7a
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-11-19 23:42:00 +0100

    Remove inconsequent nzbget config

--- nixos/nzbget.nix
+++ nixos/nzbget.nix
@@ -1,10 +1,7 @@
-{config, ...}: {
+{...}: {
   services.nzbget = {
     enable = true;
     user = "fausto";
     group = "users";
-    settings = {
-      MainDir = "${config.users.users.fausto.home}/nzbget";
-    };
   };
 }
