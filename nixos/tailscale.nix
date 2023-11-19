6530b9c239322c0f620b323c2ecf7950cd1df324
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-11-19 22:35:09 +0100

    Switch order in Caddyfile

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -10,11 +10,11 @@ in {
     enable = true;
     virtualHosts = {
       "bilbo.${host}".extraConfig = ''
-        reverse_proxy localhost:8096
-
         handle_path /grafana/* {
           reverse_proxy localhost:2342
         }
+
+        reverse_proxy localhost:8096
       '';
     };
   };
