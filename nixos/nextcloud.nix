74f6add349c44ae67d4cd24413409cb861e8c5c3
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-04-01 18:21:15 +0200

    Add nextcloud:nextcloud

--- nixos/nextcloud.nix
+++ nixos/nextcloud.nix
@@ -6,6 +6,11 @@
   host = "walrus-dorian.ts.net";
   packages = config.services.nextcloud.package.packages;
 in {
+  users.users.nextcloud = {
+    isNormalUser = true;
+    createHome = false;
+  };
+  users.groups.nextcloud = {};
   services.nextcloud = {
     enable = true;
     home = "/mnt/mirrored/nextcloud";
