72622ee3560a6878af28814e0943b431e9e21482
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-04-01 16:51:22 +0200

    Upgrade to 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "fnune's Bilbo—A media server";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
