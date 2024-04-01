6b9720595f2c27f8e87001056c745816b4ecf3a6
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-04-01 17:09:45 +0200

    Use nixos-23.11 instead of release-23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "fnune's Bilbo—A media server";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
