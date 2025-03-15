608f701bd0afc9f056ead8cc85d4b7f08cdaaccc
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-03-16 00:30:08 +0100

    File Filebrowser permissions and path

--- nixos/filebrowser.nix
+++ nixos/filebrowser.nix
@@ -1,39 +1,43 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  datadir = "/mnt/mirrored/filebrowser";
+  vardir = "/var/lib/filebrowser";
+in {
   environment.etc."filebrowser/config.json".text = ''
     {
       "port": 8080,
       "baseURL": "/filebrowser",
       "address": "127.0.0.1",
       "log": "stdout",
-      "database": "/var/lib/filebrowser/filebrowser.db",
-      "root": "/srv"
+      "database": "${vardir}/filebrowser.db",
+      "root": "${datadir}"
     }
   '';
 
   systemd.tmpfiles.rules = [
-    "d /var/lib/filebrowser 0755 filebrowser filebrowser -"
+    "d ${vardir} 0755 filebrowser users -"
+    "d ${datadir} 0755 filebrowser users -"
   ];
 
   users.users.filebrowser = {
     isSystemUser = true;
-    group = "filebrowser";
-    home = "/var/lib/filebrowser";
+    group = "users";
+    home = vardir;
     createHome = true;
   };
-  users.groups.filebrowser = {};
 
-  systemd.services.filebrowser = {
-    description = "File Browser";
-    after = ["network.target"];
-    wantedBy = ["multi-user.target"];
-
-    serviceConfig = {
-      ExecStart = "${pkgs.filebrowser}/bin/filebrowser -c /etc/filebrowser/config.json";
-      Restart = "always";
-      User = "filebrowser";
-      Group = "filebrowser";
-      WorkingDirectory = "/var/lib/filebrowser";
-      StateDirectory = "filebrowser";
+  systemd.services = {
+    filebrowser = {
+      description = "File Browser";
+      after = ["network.target"];
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        ExecStart = "${pkgs.filebrowser}/bin/filebrowser -c /etc/filebrowser/config.json";
+        Restart = "always";
+        User = "filebrowser";
+        Group = "users";
+        WorkingDirectory = vardir;
+        StateDirectory = "filebrowser";
+      };
     };
   };
 }
