bce32d4496cb0754761b9fb9c15a1249b08c8440
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-01-10 16:26:21 +0100

    Fix after intel-vaapi-driver rename

--- nixos/hardware-acceleration.nix
+++ nixos/hardware-acceleration.nix
@@ -1,15 +1,15 @@
 {pkgs, ...}: {
   nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override {enableHybridCodec = true;};
   };
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       intel-compute-runtime
       intel-media-driver
+      intel-vaapi-driver
+      libva-vdpau-driver
       libvdpau-va-gl
-      vaapiIntel
-      vaapiVdpau
     ];
   };
 }
