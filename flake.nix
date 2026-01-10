adf80b080523d1987517efe525e2661ac644f47d
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-01-10 16:22:16 +0100

    Upgrade the system

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "fnune's Bilbo—A media server";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     disko = {
       url = "github:nix-community/disko/v1.11.0";
