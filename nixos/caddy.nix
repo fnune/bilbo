c2f90d585780f471d5d49c428e5353dbcfb28b06
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-05-24 06:39:45 +0200

    Fix bad cf plugin hash

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -113,7 +113,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.4"];
-        hash = "sha256-4WF7tIx8d6O/Bd0q9GhMch8lS3nlR5N3Zg4ApA3hrKw=";
+        hash = "sha256-vNSHU7txQLs0m0UChuszURXjEoMj4r1902+1ei0/DaI=";
       };
       virtualHosts = {
         "${immich}".extraConfig = ''
