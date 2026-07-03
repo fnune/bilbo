956111c762bbfe20b12a476d5ac1e842ac2d85cd
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-07-03 13:04:01 +0200

    Upgrade packages

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -142,7 +142,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.4"];
-        hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
+        hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
       };
       virtualHosts = {
         "${immich}".extraConfig = ''
