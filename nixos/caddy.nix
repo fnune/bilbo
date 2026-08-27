aaec2826125ed322d1fee2591e7d747c4bad068c
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-08-27 11:55:02 +0200

    Update the Caddy plugin vendor hash for 2.11.4

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -155,7 +155,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.4"];
-        hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
+        hash = "sha256-PWadA5qr/gR2qDcT8l8u1Xku7LM2HIfWTLOkzezCYy0=";
       };
       virtualHosts = {
         "${immich}".extraConfig = ''
