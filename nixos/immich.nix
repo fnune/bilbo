d73232f186a11881485c5fa59fe0b533c1173010
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-09-11 20:14:23 +0200

    Run Immich 3 from nixpkgs-unstable

--- nixos/immich.nix
+++ nixos/immich.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [immich-cli exiftool];
+  environment.systemPackages = [pkgs.unstable.immich-cli pkgs.exiftool];
   services.postgresql = {
     enable = true;
     ensureUsers = [{name = "fausto";}];
   };
   services.immich = {
     enable = true;
-    package = pkgs.immich;
+    package = pkgs.unstable.immich;
     group = "users";
     mediaLocation = "/mnt/mirrored/immich";
     database = {
